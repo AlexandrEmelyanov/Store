cffe43883752da491aa0d2c4ccd945df4fae341d
Alexander Emelyanov <emelianoff.casha@mail.ru>
2023-06-10 23:15:36 +0500

    Add checkout Stripe

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -5,4 +5,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('order-canceled/', views.CanceledTemplateView.as_view(), name='order_canceled'),
+    path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
 ]
