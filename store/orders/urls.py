b0b47e99136804a7baeb2128298da8b996be81a5
Alexander Emelyanov <emelianoff.casha@mail.ru>
2023-06-12 20:44:40 +0500

    add order detail list

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from orders import views
 
 app_name = 'orders'
@@ -6,6 +7,7 @@ app_name = 'orders'
 urlpatterns = [
     path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
     path('', views.OrderListView.as_view(), name='orders_list'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order'),
     path('order-canceled/', views.CanceledTemplateView.as_view(), name='order_canceled'),
     path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
     path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
